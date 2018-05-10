eb65e593e3f58b889cf6578c2c47df7b14b6360d
王跃 <wangyue57@meituan.com>
2018-05-10 11:36:11 +0800

    文案页上滑

--- src/pages/Copywriting.vue
+++ src/pages/Copywriting.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="page">
+  <div class="page" @touchstart="touchstart" @touchend="touchend">
     <img class="page-bg" src="../assets/copywriting/pic_01.jpg">
     <img class="title" src="../assets/copywriting/pic_01.png">
     <img class="text center" src="../assets/copywriting/pic_02.png">
@@ -16,14 +16,26 @@
   export default {
     name: 'Copywriting',
     mixins: [vuexMixin],
+    data: () => ({
+      startY: null
+    }),
     methods: {
+      touchstart (event) {
+        this.startY = event.changedTouches[0].pageY
+      },
+      touchend (event) {
+        const endY = event.changedTouches[0].pageY
+        if (endY - this.startY > 5) {
+          this.moveUp()
+        }
+      },
       takeMother () {
         parseFile(this.$refs.mother.files[0], result => {
           this.moveDown()
           this.saveMotherPic(result)
         })
-      }
-    }
+      },
+    },
   }
 </script>
 
